157d73afbe1e7d4c7b56e1ce738183ef36a1bbff
ZMY <myjs5@qq.com>
2025-02-18 16:40:58 +0800

    提交dist目录

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,33 @@
 import terser from '@rollup/plugin-terser';
 
+const banner = `
+/**
+ * @name ${process.env.npm_package_name}
+ * @version ${process.env.npm_package_version}
+ * @build ${Date.now()}
+ * @author ZMY
+ * @license MIT
+ */
+`.trim();
+
+function getOutputItem(format, suffix) {
+    return {
+        file: `dist/amrplayer.${suffix}`,
+        format,
+        strict: false,
+        banner,
+        plugins: [terser()],
+    };
+}
+
 export default {
     input: 'src/main.js',
     output: [
+        getOutputItem('es', 'es.js'),
+        getOutputItem('cjs', 'cjs.js'),
         {
-            file: 'dist/amrplayer.es.js',
-            format: 'es',
-            strict: false
-        },
-        {
-            file: 'dist/amrplayer.cjs.js',
-            format: 'cjs',
-            strict: false
-        },
-        {
-            file: 'dist/amrplayer.js',
-            format: 'iife',
+            ...getOutputItem('iife', 'js'),
             name: 'AMRPlayer',
-            strict: false,
-            plugins: [terser()]
-        },
+        }
     ]
 };
